af9917a7a840d94965f28207de1642475758995f
Alhadji Oumate <alhadjioum0@gmail.com>
2025-10-21 14:54:02 +0100

    fix: correct configuration path in create_app function

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('src.config.Config')
+    app.config.from_object('.config.Config')
 
 
     db.init_app(app)
