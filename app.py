9a7ab77a2d16e3014cb9186b74ab72b3f7446bba
Alhadji Oumate <alhadjioum0@gmail.com>
2025-10-21 14:50:16 +0100

    refactor: remove unused files and restructure application layout

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from src import create_app
+from . import create_app
 
 
 app = create_app()
 
 if __name__ == '__main__':
-    from src.config import db
+    from .config import db
     with app.app_context():
         db.create_all()
     app.run(debug=True)
---
+++ blueprints.py
@@ -0,0 +1,22 @@
+# # AUTHOR: ALHADJI OUMATE
+# # STUDENT ID: 22U2033
+
+# from .routes.foods import foods_bp
+# from .routes.persons import persons_bp
+# from .routes.food_recommendation import rec_bp as food_recommendation_bp
+# from .routes.allergy_probability import rec_bp as allergy_probability_bp
+# from .routes.food_consumptions import food_consumptions_bp
+# from .routes.weekly_plan import weekly_plan_bp
+# from .routes.buffet_manage import buffet_bp
+
+def register_blueprints(app):
+    """Register Flask blueprints with the app."""
+    pass  # No blueprints registered yet
+    # Uncomment and add blueprints when routes are ready:
+    # app.register_blueprint(foods_bp)
+    # app.register_blueprint(persons_bp)
+    # app.register_blueprint(food_recommendation_bp)
+    # app.register_blueprint(allergy_probability_bp)
+    # app.register_blueprint(food_consumptions_bp)
+    # app.register_blueprint(weekly_plan_bp)
+    # app.register_blueprint(buffet_bp)
