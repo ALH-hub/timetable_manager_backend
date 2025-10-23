91fd5410c2c02ee18065d604b5e7135046a6dbfa
Alhadji Oumate <alhadjioum0@gmail.com>
2025-10-23 10:33:46 +0100

    fix: fixed the relationship between teacher and user models

--- src/models/teacher.py
+++ src/models/teacher.py
@@ -3,8 +3,9 @@ from .user import User
 
 class Teacher(User):
     __tablename__ = 'teachers'
+    id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
 
-    id = db.Column(db.Integer, primary_key=True)
+    course = db.relationship('courses', backref='teacher')
 
     def __repr__(self):
         return f'<Teacher {self.name}>'
